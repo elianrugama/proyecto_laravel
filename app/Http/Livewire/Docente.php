60ffb59e5481a08a048db56180b43730f8c4bd51
Elian Rugama <elianrugama200@gmail.com>
2022-04-28 19:30:36 -0600

    nada

---
+++ app/Http/Livewire/Docente.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Docentes;
+
+class Docente extends Component
+{
+    
+    public $nombre, $carrera, $año, $id_docentes;
+    public $modal = false;
+
+    public function render()
+    {
+        $this->docentes = Docentes::all();
+        return view('livewire.docente');
+    }
+
+    public function crear()
+    {
+        $this->limpiarCampos();
+        $this->abrirModal();
+    }
+
+    public function abrirModal() {
+        $this->modal = true;
+    }
+    public function cerrarModal() {
+        $this->modal = false;
+    }
+    public function limpiarCampos(){
+        $this->nombre = '';
+        $this->carrera = '';
+        $this->id_docentes = '';
+    }
+    public function editar($id)
+    {
+        $docente = Docente::findOrFail($id);
+        $this->id_docentes = $id;
+        $this->nombre = $docente->nombre;
+        $this->carrera = $docente->carrera;
+        $this->abrirModal();
+    }
+
+    public function borrar($id)
+    {
+        Docente::find($id)->delete();
+        session()->flash('message', 'Registro eliminado correctamente');
+    }
+
+    public function guardar()
+    {
+        Docente::updateOrCreate(['id'=>$this->id_docentes],
+            [
+                'nombre' => $this->nombre,
+                'carrera' => $this->carrera
+            ]);
+         
+         session()->flash('message',
+            $this->id_docentes ? '¡Actualización exitosa!' : '¡Alta Exitosa!');
+         
+         $this->cerrarModal();
+         $this->limpiarCampos();
+    }
+}
+
+
+
+
+
